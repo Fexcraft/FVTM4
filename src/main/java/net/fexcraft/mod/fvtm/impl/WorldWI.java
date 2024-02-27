da42f8614513b9aebb57523a94f10088325a44f7
Ferdinand Calo <fexcalo@gmail.com>
2024-02-27 11:31:50 +0100

    Create WorldWI.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/impl/WorldWI.java
@@ -0,0 +1,63 @@
+package net.fexcraft.mod.fvtm.impl;
+
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.uni.world.EntityW;
+import net.fexcraft.mod.uni.world.StateWrapper;
+import net.fexcraft.mod.uni.world.WorldW;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class WorldWI extends WorldW {
+
+	private Level level;
+
+	public WorldWI(Level world){
+		super();
+		level = world;
+	}
+
+	@Override
+	public boolean isClient(){
+		return level.isClientSide;
+	}
+
+	@Override
+	public boolean isTilePresent(V3I pos){
+		return level.getBlockEntity(new BlockPos(pos.x, pos.y, pos.z)) != null;
+	}
+
+	@Override
+	public Object getBlockEntity(V3I pos){
+		return level.getBlockEntity(new BlockPos(pos.x, pos.y, pos.z));
+	}
+
+	@Override
+	public <W> W local(){
+		return (W)level;
+	}
+
+	@Override
+	public Object direct(){
+		return level;
+	}
+
+	@Override
+	public void setBlockState(V3I pos, StateWrapper state, int flag){
+		//
+	}
+
+	@Override
+	public void spawnBlockSeat(V3D add, EntityW player){
+		//
+	}
+
+	@Override
+	public int dim(){
+		return 0;
+	}
+
+}
