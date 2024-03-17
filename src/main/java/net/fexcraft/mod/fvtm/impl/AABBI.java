2ae34029f06b513ca042ff87cc41c013146ff2b4
Ferdinand Calo <fexcalo@gmail.com>
2024-03-17 11:36:34 +0100

    Update AABBI.java

--- src/main/java/net/fexcraft/mod/fvtm/impl/AABBI.java
+++ src/main/java/net/fexcraft/mod/fvtm/impl/AABBI.java
@@ -10,37 +10,51 @@ import net.minecraft.world.phys.Vec3;
  */
 public class AABBI extends net.fexcraft.mod.fvtm.data.block.AABB {
 
-    private AABB aabb;
+	private AABB aabb;
 
-    @Override
-    public net.fexcraft.mod.fvtm.data.block.AABB set(float sx, float sy, float sz, float ex, float ey, float ez){
-        aabb = new AABB(sx, sy, sz, ex, ey, ez);
-        return this;
-    }
+	public AABBI(){
+	}
 
-    @Override
-    public <AB> AB local(){
-        return (AB)aabb;
-    }
+	public AABBI(AABB albb){
+		aabb = albb;
+	}
 
-    @Override
-    public Object direct(){
-        return aabb;
-    }
+	@Override
+	public net.fexcraft.mod.fvtm.data.block.AABB set(float sx, float sy, float sz, float ex, float ey, float ez){
+		aabb = new AABB(sx, sy, sz, ex, ey, ez);
+		return this;
+	}
 
-    @Override
-    public <AB> AB offset(int x, int y, int z){
-        return (AB)aabb;//TODO.offset(x, y, z);
-    }
+	@Override
+	public <AB> AB local(){
+		return (AB)aabb;
+	}
 
-    @Override
-    public boolean contains(V3D vec){
-        return aabb.contains(vec.x, vec.y, vec.z);
-    }
+	@Override
+	public Object direct(){
+		return aabb;
+	}
 
-    @Override
-    public boolean contains(Object vec){
-        return aabb.contains((Vec3)vec);
-    }
+	@Override
+	public net.fexcraft.mod.fvtm.data.block.AABB offset(int x, int y, int z){
+		AABB nbb = new AABB(aabb.minX + x, aabb.minY + y, aabb.minZ + z, aabb.maxX + x, aabb.maxY + y, aabb.maxZ + z);
+		return new AABBI(nbb);
+	}
+
+	@Override
+	public net.fexcraft.mod.fvtm.data.block.AABB offset(V3D vec){
+		AABB nbb = new AABB(aabb.minX + vec.x, aabb.minY + vec.y, aabb.minZ + vec.z, aabb.maxX + vec.x, aabb.maxY + vec.y, aabb.maxZ + vec.z);
+		return new AABBI(nbb);
+	}
+
+	@Override
+	public boolean contains(V3D vec){
+		return aabb.contains(vec.x, vec.y, vec.z);
+	}
+
+	@Override
+	public boolean contains(Object vec){
+		return aabb.contains((Vec3)vec);
+	}
 
 }
