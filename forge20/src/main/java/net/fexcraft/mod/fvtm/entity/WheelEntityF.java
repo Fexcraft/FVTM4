106261d2f21a5e376a3f54ac57c35f9d1517ff08
Ferdinand Calo <fexcalo@gmail.com>
2024-03-24 22:05:26 +0100

    Create WheelEntityF.java

---
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/entity/WheelEntityF.java
@@ -0,0 +1,27 @@
+package net.fexcraft.mod.fvtm.entity;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.entity.IEntityAdditionalSpawnData;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class WheelEntityF extends WheelEntity implements IEntityAdditionalSpawnData {
+
+	public WheelEntityF(EntityType<WheelEntity> type, Level level){
+		super(type, level);
+	}
+
+	@Override
+	public void writeSpawnData(FriendlyByteBuf buffer){
+		super.writeSpawnData(buffer);
+	}
+
+	@Override
+	public void readSpawnData(FriendlyByteBuf buffer){
+		super.readSpawnData(buffer);
+	}
+
+}
