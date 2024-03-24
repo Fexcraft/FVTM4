1a456c52ef2a7958d7364055cd1cad99cffb2324
Ferdinand Calo <fexcalo@gmail.com>
2024-03-24 22:05:32 +0100

    Create WheelEntityN.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/entity/WheelEntityN.java
@@ -0,0 +1,27 @@
+package net.fexcraft.mod.fvtm.entity;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.entity.IEntityWithComplexSpawn;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class WheelEntityN extends WheelEntity implements IEntityWithComplexSpawn {
+
+	public WheelEntityN(EntityType<WheelEntity> type, Level level){
+		super(type, level);
+	}
+
+	@Override
+	public void writeSpawnData(FriendlyByteBuf buffer){
+		super.writeSpawnData(buffer);
+	}
+
+	@Override
+	public void readSpawnData(FriendlyByteBuf buffer){
+		super.readSpawnData(buffer);
+	}
+
+}
