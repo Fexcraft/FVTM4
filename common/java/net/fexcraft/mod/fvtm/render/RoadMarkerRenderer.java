3fa4d687456f79b3c99850700075ffb5cbf94a4c
Ferdinand Calo <fexcalo@gmail.com>
2024-04-21 22:58:50 +0200

    Update RoadMarkerRenderer.java

--- common/java/net/fexcraft/mod/fvtm/render/RoadMarkerRenderer.java
+++ common/java/net/fexcraft/mod/fvtm/render/RoadMarkerRenderer.java
@@ -1,18 +1,27 @@
 package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.fexcraft.mod.fvtm.FvtmRegistry;
+import net.fexcraft.lib.common.Static;
+import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.mod.fvtm.entity.RoadMarker;
-import net.fexcraft.mod.fvtm.entity.WheelEntity;
+import net.fexcraft.mod.fvtm.model.entity.RoadMarkerModel;
+import net.fexcraft.mod.fvtm.sys.road.RoadPlacingUtil;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import org.joml.Quaternionf;
+
+import static net.fexcraft.mod.fvtm.render.Renderer120.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class RoadMarkerRenderer extends EntityRenderer<RoadMarker> {
+
+	private ResourceLocation texture = new ResourceLocation("fvtm:textures/entity/roadmarker.png");
+	public static final RGB CYAN = new RGB(0, 255, 255);
 
 	public RoadMarkerRenderer(EntityRendererProvider.Context context){
 		super(context);
@@ -20,13 +29,36 @@ public class RoadMarkerRenderer extends EntityRenderer<RoadMarker> {
 	}
 
 	@Override
-	public void render(RoadMarker deco, float yaw, float tick, PoseStack pose, MultiBufferSource buffer, int light){
-		//
+	public void render(RoadMarker marker, float yaw, float tick, PoseStack pose, MultiBufferSource buffer, int light){
+		pose.pushPose();
+		Renderer120.pose = pose;
+		Renderer120.buffer = buffer;
+		Renderer120.light = light;
+		Renderer120.rentype = RenderType.entityCutout(texture);
+		pose.mulPose(new Quaternionf().rotateAxis(Static.rad180, AZ));
+		RoadMarkerModel.INST.marker.render();
+		if(marker.queueid == null){
+			Renderer120.setColor(RGB.BLACK);
+			RoadMarkerModel.INST.arrow.render();
+		}
+		else{
+			RoadPlacingUtil.NewRoad road = RoadPlacingUtil.QUEUE.get(marker.queueid);
+			if(road != null){
+				int index = road.indexOf(marker.position);
+				boolean arrow = index == road.selected || index == 0 || index == road.points.size() - 1;
+				if(arrow){
+					Renderer120.setColor(index == road.selected ? CYAN : index == 0 ? RGB.GREEN : RGB.RED);
+					RoadMarkerModel.INST.arrow.render();
+				}
+			}
+		}
+		Renderer120.resetColor();
+		pose.popPose();
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(RoadMarker entity){
-		return FvtmRegistry.WHITE_TEXTURE.local();
+		return texture;
 	}
 
 }
