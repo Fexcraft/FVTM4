757b3b7b56274a0ef27f0547e0fedb3ef68347a8
Ferdinand Calo <fexcalo@gmail.com>
2024-04-21 13:48:45 +0200

    Create RoadToolConImpl.java

---
+++ common/java/net/fexcraft/mod/fvtm/ui/RoadToolConImpl.java
@@ -0,0 +1,45 @@
+package net.fexcraft.mod.fvtm.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.impl.SWIE;
+import net.fexcraft.mod.fvtm.ui.road.RoadToolCon;
+import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
+import net.minecraft.world.item.ItemStack;
+
+/**
+ * @author Ferdinand Calo' (FEX___96
+ */
+public class RoadToolConImpl extends RoadToolCon {
+
+	private RoadInventory inv = new RoadInventory();
+	private SWIE wrapper = new SWIE(ItemStack.EMPTY);
+
+	public RoadToolConImpl(JsonMap map, EntityW player, V3I pos){
+		super(map, player, pos);
+	}
+
+	@Override
+	public Object getInventory(){
+		return inv;
+	}
+
+	@Override
+	public void setInventoryContent(int index, TagCW com){
+		inv.setItem(index, ItemStack.of(com.local()));
+	}
+
+	@Override
+	public StackWrapper getInventoryContent(int index){
+		wrapper.stack = inv.getItem(index);
+		return wrapper;
+	}
+
+	@Override
+	public boolean isInventoryEmpty(int at){
+		return inv.getItem(at).isEmpty();
+	}
+
+}
