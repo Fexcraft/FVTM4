abb6592dbdad229598a7a7852b1ffbe94967c32e
Ferdinand Calo <fexcalo@gmail.com>
2024-04-21 14:09:51 +0200

    Create RoadToolCustomConImpl.java

---
+++ common/java/net/fexcraft/mod/fvtm/ui/RoadToolCustomConImpl.java
@@ -0,0 +1,72 @@
+package net.fexcraft.mod.fvtm.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.impl.SWIE;
+import net.fexcraft.mod.fvtm.sys.uni.FvtmWorld;
+import net.fexcraft.mod.fvtm.ui.road.RoadToolCustomCon;
+import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.uimpl.UniCon;
+import net.fexcraft.mod.uni.world.EntityW;
+import net.minecraft.world.item.ItemStack;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class RoadToolCustomConImpl extends RoadToolCustomCon {
+
+	protected RoadInventory inv;
+	protected SWIE wrapper = new SWIE(ItemStack.EMPTY);
+
+	public RoadToolCustomConImpl(JsonMap map, EntityW player, V3I pos){
+		super(map, player, pos);
+		inv = new RoadInventory(size[0] >= 9 ? 9 : size[0]);
+	}
+
+	@Override
+	public void init(){
+		int is = size[0] > 9 ? 9 : size[0];
+		for(int i = 0; i < is; i++){
+			((UniCon)root).addSlot(new RoadInventory.RoadSlot((FvtmWorld)player.getWorld(), inv, i, 88 - offset + 1 + i * 18, 8, true, pos.x > 0));
+		}
+		fillStacks();
+	}
+
+	@Override
+	protected void fillStacks(){
+		if(!stack.getTag().has(tagname)) return;
+		TagCW compound = stack.getTag().getCompound(tagname);
+		for(int i = 0; i < 9; i++){
+			int j = i + scroll;
+			if(j >= size[0]) break;
+			if(!compound.has("Block" + j)){
+				inv.setItem(i, ItemStack.EMPTY);
+				continue;
+			}
+			inv.setItem(i, ItemStack.of(compound.getCompound("Block" + j).local()));
+		}
+	}
+
+	@Override
+	public Object getInventory(){
+		return inv;
+	}
+
+	@Override
+	public void setInventoryContent(int index, TagCW com){
+		inv.setItem(index, ItemStack.of(com.local()));
+	}
+
+	@Override
+	public StackWrapper getInventoryContent(int index){
+		wrapper.stack = inv.getItem(index);
+		return wrapper;
+	}
+
+	@Override
+	public boolean isInventoryEmpty(int at){
+		return inv.getItem(at).isEmpty();
+	}
+
+}
