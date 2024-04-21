52266e566ec8dfcec1294ccc17df633478792ab5
Ferdinand Calo <fexcalo@gmail.com>
2024-04-21 23:13:51 +0200

    Update WheelEntityF.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/entity/WheelEntityF.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/entity/WheelEntityF.java
@@ -1,9 +1,12 @@
 package net.fexcraft.mod.fvtm.entity;
 
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.entity.IEntityAdditionalSpawnData;
+import net.minecraftforge.network.NetworkHooks;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -23,5 +26,10 @@ public class WheelEntityF extends WheelEntity implements IEntityAdditionalSpawnD
 	public void readSpawnData(FriendlyByteBuf buffer){
 		super.readSpawnData(buffer);
 	}
+
+	@Override
+    public Packet<ClientGamePacketListener> getAddEntityPacket() {
+        return NetworkHooks.getEntitySpawningPacket(this);
+    }
 
 }
