babac92db48b76fc10a6a3550c0eb9612fda3139
Ferdinand Calo <fexcalo@gmail.com>
2024-04-21 14:12:16 +0200

    Update FVTM20.java

--- common/java/net/fexcraft/mod/fvtm/FVTM20.java
+++ common/java/net/fexcraft/mod/fvtm/FVTM20.java
@@ -63,8 +63,8 @@ public class FVTM20 {
 		//
 		UniReg.registerUI(UIKey.ROAD_TOOL.key, RoadToolUI.class);
 		UniReg.registerMenu(UIKey.ROAD_TOOL.key, "assets/fvtm/uis/road_tool", RoadToolConImpl.class);
-		UniReg.registerUI(UIKey.ROAD_TOOL.key, RoadToolCustomUI.class);
-		UniReg.registerMenu(UIKey.ROAD_TOOL.key, "assets/fvtm/uis/road_tool_custom", RoadToolCustomConImpl.class);
+		UniReg.registerUI(UIKey.ROAD_TOOL_CUSTOM.key, RoadToolCustomUI.class);
+		UniReg.registerMenu(UIKey.ROAD_TOOL_CUSTOM.key, "assets/fvtm/uis/road_tool_custom", RoadToolCustomConImpl.class);
 		//
 		UniReg.registerUI(UIKey.VEHICLE_MAIN.key, VehicleMain.class);
 		UniReg.registerMenu(UIKey.VEHICLE_MAIN.key, "assets/fvtm/uis/vehicle_main", VehicleMainCon.class);
