cce5145884ecd03c5489fe268b94d3126919e6ce
Ferdinand Calo <fexcalo@gmail.com>
2024-06-06 23:10:14 +0200

    Update VehicleLiftBlock.java

--- common/java/net/fexcraft/mod/fvtm/block/VehicleLiftBlock.java
+++ common/java/net/fexcraft/mod/fvtm/block/VehicleLiftBlock.java
@@ -1,9 +1,14 @@
 package net.fexcraft.mod.fvtm.block;
 
 import com.mojang.serialization.MapCodec;
+import net.fexcraft.mod.fvtm.FvtmGetters;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -12,7 +17,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class VehicleLiftBlock extends BaseEntityBlock {
+public class VehicleLiftBlock extends Block implements EntityBlock {
 
 	public VehicleLiftBlock(){
 		super(Properties.of().noOcclusion().explosionResistance(64).strength(2));
@@ -24,13 +29,20 @@ public class VehicleLiftBlock extends BaseEntityBlock {
 	}
 
 	@Override
-	protected MapCodec<? extends BaseEntityBlock> codec(){
-		return null;
+	public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState){
+		return FvtmGetters.LIFT_ENTITY.get().create(pPos, pState);
 	}
 
 	@Override
-	public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState){
-		return null;
+	public RenderShape getRenderShape(BlockState pState){
+		return RenderShape.INVISIBLE;
+	}
+
+	@Override
+	public boolean triggerEvent(BlockState state, Level level, BlockPos pos, int i, int j){
+		super.triggerEvent(state, level, pos, i, j);
+		BlockEntity be = level.getBlockEntity(pos);
+		return be == null ? false : be.triggerEvent(i, j);
 	}
 
 }
