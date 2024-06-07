5f4a582c7f3a98145450818877eea398a250f6ba
Ferdinand Calo <fexcalo@gmail.com>
2024-06-07 11:19:48 +0200

    Update ForgeClientEvents.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/event/ForgeClientEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/event/ForgeClientEvents.java
@@ -5,10 +5,8 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.fexcraft.mod.fvtm.render.Renderer120;
 import net.fexcraft.mod.fvtm.util.DebugUtils;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.client.event.RenderPlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,9 +19,9 @@ public class ForgeClientEvents {
 
 	//@SubscribeEvent
 	public static void onPlayerRender(RenderPlayerEvent.Pre event){
-		Renderer120.pose = event.getPoseStack();
-		Renderer120.buffer = event.getMultiBufferSource();
-		Renderer120.light = event.getPackedLight();
+		//Renderer120.pose = event.getPoseStack();
+		//Renderer120.buffer = event.getMultiBufferSource();
+		//Renderer120.light = event.getPackedLight();
 		if(!event.getRenderer().getModel().body.hasChild("fvtm")){
 			event.getRenderer().getModel().body.children.put("fvtm", new ModelPart(new ArrayList<>(), new HashMap<>()) {
 				@Override
@@ -33,9 +31,8 @@ public class ForgeClientEvents {
 
 				@Override
 				public void render(PoseStack pose, VertexConsumer cons, int i, int j, float k, float l, float m, float n){
-					Renderer120.pose = pose;
-					Renderer120.cons = cons;
-					Renderer120.buffer = null;
+					Renderer120.set(pose, cons, i, j);
+					Renderer120.set(RenderType.lines());
 					DebugUtils.SPHERE.render();
 				}
 			});
