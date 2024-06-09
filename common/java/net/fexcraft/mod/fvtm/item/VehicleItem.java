4b1dd366f76d977d2c84964067bdf1e499f06377
Ferdinand Calo <fexcalo@gmail.com>
2024-06-09 10:04:27 +0200

    Update VehicleItem.java

--- common/java/net/fexcraft/mod/fvtm/item/VehicleItem.java
+++ common/java/net/fexcraft/mod/fvtm/item/VehicleItem.java
@@ -1,8 +1,9 @@
 package net.fexcraft.mod.fvtm.item;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.fexcraft.lib.common.Static;
+import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.mod.fvtm.FvtmGetters;
-import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.data.ContentItem.ContentDataItem;
 import net.fexcraft.mod.fvtm.data.ContentType;
 import net.fexcraft.mod.fvtm.data.attribute.Attribute;
@@ -13,6 +14,8 @@ import net.fexcraft.mod.fvtm.entity.RootVehicle;
 import net.fexcraft.mod.fvtm.function.part.EngineFunction;
 import net.fexcraft.mod.fvtm.function.part.TransmissionFunction;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
+import net.fexcraft.mod.fvtm.model.content.VehicleModel;
+import net.fexcraft.mod.fvtm.render.RVRenderer;
 import net.fexcraft.mod.fvtm.render.Renderer120;
 import net.fexcraft.mod.fvtm.util.GenericUtils;
 import net.fexcraft.mod.uni.item.StackWrapper;
@@ -32,10 +35,14 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.minecraftforge.common.util.NonNullLazy;
+import org.joml.Quaternionf;
 
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.function.Consumer;
+
+import static net.fexcraft.lib.common.Static.eighth;
+import static net.fexcraft.mod.fvtm.render.Renderer120.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -128,15 +135,44 @@ public class VehicleItem extends Item implements ContentDataItem<Vehicle, Vehicl
 	@Override
 	public void initializeClient(Consumer<IClientItemExtensions> consumer){
 		consumer.accept(new IClientItemExtensions() {
-			private static final NonNullLazy<BlockEntityWithoutLevelRenderer> renderer = NonNullLazy.of(() -> new BlockEntityWithoutLevelRenderer(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels()){
+			private static final NonNullLazy<BlockEntityWithoutLevelRenderer> renderer = NonNullLazy.of(() -> new BlockEntityWithoutLevelRenderer(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels()) {
 				@Override
 				public void renderByItem(ItemStack stack, ItemDisplayContext context, PoseStack pose, MultiBufferSource src, int v0, int v1){
-					if(!stack.hasTag()) return;
-					VehicleData data = FvtmResources.getVehicleData(stack.getTag());
+					VehicleData data = FvtmGetters.VEHDATACACHE.apply(stack).getContent();
 					if(data != null && data.getType().getModel() != null){
+						VehicleModel model = (VehicleModel)data.getType().getModel();
 						Renderer120.set(pose, src, v0);
 						Renderer120.set(RenderType.entityCutout(data.getCurrentTexture().local()));
-						data.getType().getModel().render(DefaultModel.RENDERDATA);
+						pose.pushPose();
+						//
+						pose.translate(0.5, 0.5, 0);
+						Vec3f translate = model.item_translate.get(context.name());
+						pose.translate(translate.x, translate.y, translate.z);
+						if(data.getType().isTrailer() && !data.getType().getVehicleType().isRailVehicle()){
+							if(context == ItemDisplayContext.GUI){
+								pose.translate(-.375, -.375, 0);
+							}
+							else if(context == ItemDisplayContext.FIRST_PERSON_LEFT_HAND || context == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND){
+								pose.translate(0, 0, -0.5);
+							}
+						}
+						Vec3f rotate = model.item_rotate.get(context.name());
+						pose.mulPose(new Quaternionf()
+							.rotateAxis(Static.toRadians(rotate.y), AY)
+							.rotateAxis(Static.toRadians(rotate.x), AX)
+							.rotateAxis(Static.toRadians(rotate.z), AZ));
+						Vec3f scale = model.item_scale.get(context.name());
+						pose.scale(scale.x, scale.y, scale.z);
+						//
+						pose.pushPose();
+						pose.mulPose(new Quaternionf().rotateAxis(-Static.rad90, AY));
+						model.render(DefaultModel.RENDERDATA.set(data, null, null, false, 0));
+						if(data.getParts().size() > 0){
+							RVRenderer.renderPoint(pose, data.getRotationPoint("vehicle"), null, data, null, 0);
+						}
+						pose.popPose();
+						//
+						pose.popPose();
 					}
 				}
 			});
