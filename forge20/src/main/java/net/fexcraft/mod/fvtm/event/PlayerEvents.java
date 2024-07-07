f6381769f39dcb4702c40756200f2dd935f8afd4
Ferdinand Calo <fexcalo@gmail.com>
2024-07-07 17:54:23 +0200

    Update PlayerEvents.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/event/PlayerEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/event/PlayerEvents.java
@@ -1,24 +1,10 @@
 package net.fexcraft.mod.fvtm.event;
 
-import net.fexcraft.mod.fcl.util.PassengerUtil;
-import net.fexcraft.mod.fvtm.entity.RootVehicle;
-import net.fexcraft.mod.fvtm.handler.InteractionHandler;
 import net.fexcraft.mod.fvtm.sys.road.RoadPlacingCache;
 import net.fexcraft.mod.fvtm.sys.road.RoadPlacingUtil;
-import net.fexcraft.mod.fvtm.sys.uni.KeyPress;
-import net.fexcraft.mod.fvtm.sys.uni.Passenger;
-import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
-import net.fexcraft.mod.uni.item.StackWrapper;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import static net.fexcraft.mod.fvtm.event.ClientEvents.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
