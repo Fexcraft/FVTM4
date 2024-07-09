77fef0d970cad518f35d5396b46b08ce77bef9e2
Ferdinand Calo <fexcalo@gmail.com>
2024-07-09 19:24:28 +0200

    Update ItemRenderers.java

--- common/java/net/fexcraft/mod/fvtm/render/ItemRenderers.java
+++ common/java/net/fexcraft/mod/fvtm/render/ItemRenderers.java
@@ -3,11 +3,10 @@ package net.fexcraft.mod.fvtm.render;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.common.math.Vec3f;
-import net.fexcraft.mod.fvtm.FvtmGetters;
-import net.fexcraft.mod.fvtm.FvtmLogger;
-import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
 import net.fexcraft.mod.fvtm.model.content.VehicleModel;
+import net.fexcraft.mod.fvtm.util.VehItemApp;
+import net.fexcraft.mod.uni.item.StackWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -28,17 +27,17 @@ public class ItemRenderers {
 	public static final NonNullLazy<BlockEntityWithoutLevelRenderer> VEHICLE_RENDERER = NonNullLazy.of(() -> new BlockEntityWithoutLevelRenderer(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels()) {
 		@Override
 		public void renderByItem(ItemStack stack, ItemDisplayContext context, PoseStack pose, MultiBufferSource src, int v0, int v1){
-			VehicleData data = FvtmGetters.VEHDATACACHE.apply(stack).getContent();
-			if(data != null && data.getType().getModel() != null){
-				VehicleModel model = (VehicleModel)data.getType().getModel();
+			VehItemApp via = StackWrapper.wrapAndGetApp(stack, VehItemApp.class);
+			if(via != null && via.data != null && via.data.getType().getModel() != null){
+				VehicleModel model = (VehicleModel)via.data.getType().getModel();
 				Renderer120.set(pose, src, v0);
-				Renderer120.set(RenderType.entityCutout(data.getCurrentTexture().local()));
+				Renderer120.set(RenderType.entityCutout(via.data.getCurrentTexture().local()));
 				pose.pushPose();
 				//
 				pose.translate(0.5, 0.5, 0);
 				Vec3f translate = model.item_translate.get(context.name());
 				pose.translate(translate.x, translate.y, translate.z);
-				if(data.getType().isTrailer() && !data.getType().getVehicleType().isRailVehicle()){
+				if(via.data.getType().isTrailer() && !via.data.getType().getVehicleType().isRailVehicle()){
 					if(context == ItemDisplayContext.GUI){
 						pose.translate(-.375, -.375, 0);
 					}
@@ -56,9 +55,9 @@ public class ItemRenderers {
 				//
 				pose.pushPose();
 				pose.mulPose(new Quaternionf().rotateAxis(-Static.rad90, AY));
-				model.render(DefaultModel.RENDERDATA.set(data, null, null, false, 0));
-				if(data.getParts().size() > 0){
-					RVRenderer.renderPoint(pose, data.getRotationPoint("vehicle"), null, data, null, 0);
+				model.render(DefaultModel.RENDERDATA.set(via.data, null, null, false, 0));
+				if(via.data.getParts().size() > 0){
+					RVRenderer.renderPoint(pose, via.data.getRotationPoint("vehicle"), null, via.data, null, 0);
 				}
 				pose.popPose();
 				//
